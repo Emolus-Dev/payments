c83253a398d853590d7d63ccf26f94afb6f8ce1e
monroy95 <m.monroyc22@gmail.com>
2024-06-05 22:33:25 +0000

    feat: filtro pasarelas permitidas en payment gateway

--- payments/payments/doctype/payment_gateway/payment_gateway.js
+++ payments/payments/doctype/payment_gateway/payment_gateway.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Payment Gateway', {
-	refresh: function(frm) {
-
-	}
+frappe.ui.form.on("Payment Gateway", {
+  onload: function (frm) {
+    frm.set_query("gateway_settings", () => {
+      return {
+        filters: {
+          name: [
+            "in",
+            [
+              "PowerTranz Settings",
+              "ePayServer Settings",
+              "Visanet Settings",
+              "Stripe Settings",
+            ],
+          ],
+        },
+      };
+    });
+  },
+  refresh: function (frm) {},
 });
