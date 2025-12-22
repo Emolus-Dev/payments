5fd00f53df604cdb154c9695823d916c65fb8959
monroy95 <m.monroyc22@gmail.com>
2025-12-22 10:32:12 -0600

    refactor(gateway): agrega NeoPay Settings a lista de configuraciones

--- payments/payments/doctype/payment_gateway/payment_gateway.js
+++ payments/payments/doctype/payment_gateway/payment_gateway.js
@@ -14,6 +14,7 @@ frappe.ui.form.on("Payment Gateway", {
               "ePayServer Settings",
               "Visanet Settings",
               "Stripe Settings",
+              "NeoPay Settings",
             ],
           ],
         },
